9871f6c66c63edc4af0ce1187618a3589bfa1b99
sainath gaidhankar <sainathgaidhankar@gmail.com>
2024-10-29 11:05:24 +0530

    portfolio

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react-tilt']
+    }
+  },
+  
+  plugins: [react()],
+  base: "/React_portfolio/"
+})
+
+
